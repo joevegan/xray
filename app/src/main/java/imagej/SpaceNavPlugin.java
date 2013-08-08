6528a605708670269237c108671bbbc26b5be486
wilsrobi <wilsrobi@134.50.196.169>
2013-08-08 00:34:48 -0600

    building class..

--- app/src/main/java/imagej/SpaceNavPlugin.java
+++ app/src/main/java/imagej/SpaceNavPlugin.java
@@ -10,6 +10,7 @@ import imagej.ui.UIService;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 import imagej.display.event.input.InputEvent;
+//import SpaceNav; // needed for the 3DConnexion Mouse APIs
 
 @Plugin(type = Command.class, menuPath = "Help>SpaceNav 3D Mouse")
 public class SpaceNavPlugin implements Command {
@@ -18,7 +19,19 @@ public class SpaceNavPlugin implements Command {
 	@Parameter
 	private UIService uiService;
         private InputEvent ijInput;
-
+        
+        //private SpaceNav spaceNav;
+        
+        public SpaceNavPlugin(){
+            /* 
+            spaceNav = new SpaceNav();
+            spaceNav.waitForEvent();
+             
+            */ 
+        }
+        public void SpaceNavEventOccured(){
+            
+        }
 
 	@Override
 	public void run() {
