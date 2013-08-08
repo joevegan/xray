e4e22821879db1e7ddf3a05cacf5c25693093271
wilsrobi <wilsrobi@134.50.196.169>
2013-08-07 23:50:35 -0600

    building class..

--- app/src/main/java/imagej/SpaceNavPlugin.java
+++ app/src/main/java/imagej/SpaceNavPlugin.java
@@ -3,21 +3,25 @@
  * and open the template in the editor.
  */
 package imagej;
+
 import imagej.command.Command;
 import imagej.ui.UIService;
 
-
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
+import imagej.display.event.input.InputEvent;
 
-@Plugin(type = Command.class, menuPath = "Help>SpaceNavigator Support")
-public class SpaceNavPlugin {
-    
-    @Parameter
-    private UIService uiService;
-    
-    @Override
-    public void run() {
-        uiService.showDialog("SpaceNav3D Mouse Enabled");
-    }
+@Plugin(type = Command.class, menuPath = "Help>SpaceNav 3D Mouse")
+public class SpaceNavPlugin implements Command {
+
+
+	@Parameter
+	private UIService uiService;
+        private InputEvent ijInput;
+
+
+	@Override
+	public void run() {
+		uiService.showDialog("3D Mouse Enabled");
+	}
 }
